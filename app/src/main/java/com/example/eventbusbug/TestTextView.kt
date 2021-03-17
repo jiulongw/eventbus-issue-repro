cb5586785d54916c629f034c69412aa05c7ac780
Jiulong Wang <jiulongw@gmail.com>
2021-03-17 12:30:54 -0700

    Add EventBus demo code

--- app/src/main/java/com/example/eventbusbug/TestTextView.kt
+++ app/src/main/java/com/example/eventbusbug/TestTextView.kt
@@ -4,11 +4,16 @@ import android.content.Context
 import android.util.AttributeSet
 import android.util.Log
 import androidx.appcompat.widget.AppCompatTextView
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
 class TestTextView : AppCompatTextView {
     companion object {
         const val TAG = "TestTextView"
     }
+
+    data class Event(val value: Int)
 
     constructor(context: Context) : super(context)
 
@@ -22,13 +27,22 @@ class TestTextView : AppCompatTextView {
 
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
+        EventBus.getDefault().register(this)
 
         Log.i(TAG, "Attached.")
+
+        EventBus.getDefault().post(Event(123))
     }
 
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
+        EventBus.getDefault().unregister(this)
 
         Log.i(TAG, "Detached.")
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun foo(e: Event) {
+        Log.i(TAG, "bar: ${e.value}")
     }
 }
